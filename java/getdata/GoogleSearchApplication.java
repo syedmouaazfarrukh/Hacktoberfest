77033c8ff8a833a325a0fe1ff697895038cb8b9e
Deepak <60574055+deepakstark01@users.noreply.github.com>
2023-10-20 00:26:07 +0530

    Create GoogleSearchApplication.java
    FUlly work done

---
+++ java/getdata/GoogleSearchApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleSearchApplication.class, args);
+    }
+}
